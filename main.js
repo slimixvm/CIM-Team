b71424177423f99a7834fbbc88656033959fc14f
slimixvm <130755774+slimixvm@users.noreply.github.com>
2023-04-18 01:30:19 +0300

    Update main.js

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
             // Replace the following object with your actual user data
             const validUsers = {
-                'admin': 'password123',
+                '3rmY@kLS#H0e6': 'iNH84K5MUA&%P',
             };
 
             if (validUsers[username] && validUsers[username] === password) {
